2b5a6309076ca473234b999bb7ab2a9fb59d55fc
Avard3 <iurriy.zhur@gmail.com>
2021-09-16 11:56:25 +0300

    parser and db template

---
+++ db.py
@@ -0,0 +1,22 @@
+class Table:
+    def __init__(self, name):
+        name = name
+
+    def insert(self):
+        pass
+
+    def select(self):
+        pass
+
+    def delete(self):
+        pass
+
+class DB:
+
+    def __init__(self):
+        pass
+
+    def create(self, name):
+        table = Table(name)
+
+
---
+++ reading.py
@@ -0,0 +1,49 @@
+import re
+
+# add "" support, and ignore ‘ ‘, ‘\t‘, ‘\r‘, ‘\n‘
+
+
+def many_lines_input():
+    contents = []
+    while True:
+        line = input()
+
+        # if line[-1] == ";":
+        #     break
+        if line.find(";") != -1:
+            line = line[:line.find(";")]
+            contents.append(line)
+            break
+        else:
+            contents.append(line)
+
+    str1 = " ".join(contents)
+    return " ".join(str1.split())
+#
+# print(many_lines_input())
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+# how to get token from string(column name, index)
+
+
+def read_create(command):
+    pattern = "(CREATE|create)\s[A-Za-z_\d]*\s\(([A-Za-z0-9_]*,?(\s(INDEXED|indexed),)?\s?)*\)"
+    if re.match(pattern, command):
+        table_name = command.split()[1]
+        columns_name = re.split(r"%s" % table_name, command)[1]
+    else:
+        print("invalid syntax")
+    return table_name, columns_name
+
+
+
+
+print(find_between("CREATE cats (idgggggg, name, favourite_food indexed)", "(", ")"))
+print(read_create("CREATE cats (idgggggg, name, favourite_food indexed)"))
+# print(re.sub(r"\([A-Za-z0-9,\s]*\)", ' ', "CREATE dogs (idggggggg, name, favourite_food indexed)"))
