d61bdc764c1e1a54410180db3dcb707693d43675
Avard3 <iurriy.zhur@gmail.com>
2021-09-20 17:49:00 +0300

    add insert parser

--- reading.py
+++ reading.py
@@ -8,21 +8,20 @@ def many_lines_input():
     while True:
         line = input()
 
-        # if line[-1] == ";":
-        #     break
+# end with ;
         if line.find(";") != -1:
             line = line[:line.find(";")]
             contents.append(line)
             break
         else:
             contents.append(line)
-
+# spaces del
     str1 = " ".join(contents)
     return " ".join(str1.split())
 #
 # print(many_lines_input())
 
-
+# help func to
 def find_between(s, first, last):
     try:
         start = s.index(first) + len(first)
@@ -31,11 +30,11 @@ def find_between(s, first, last):
     except ValueError:
         return ""
 
-# how to get token from string(column name, index)
+
 
 
 def read_create(command):
-    pattern = "(CREATE|create)\s[A-Za-z_\d]*\s\(([A-Za-z0-9_]*,?(\s(INDEXED|indexed),)?\s?)*\)"
+    pattern = "(CREATE|create)\s[A-Za-z_\d]+\s\(([A-Za-z0-9_]+,?(\s(INDEXED|indexed),)?\s?)+\)"
     if re.match(pattern, command):
         table_name = command.split()[1]
         columns_name = find_between(command, "(", ")").split(", ")
@@ -52,7 +51,26 @@ def read_create(command):
 
 
 
-_, names, name = read_create("CREATE cats (idgggggg, name, favourite_food indexed)")
+# _, names, name = read_create("CREATE cats (idgggggg, name, favourite_food indexed)")
 
-print(read_create("CREATE cats (idgggggg, name, favourite_food indexed)"))
+# print(read_create("CREATE cats (idgggggg, name, favourite_food indexed)"))
 # print(re.sub(r"\([A-Za-z0-9,\s]*\)", ' ', "CREATE dogs (idggggggg, name, favourite_food indexed)"))
+
+
+def read_insert(command):
+    pattern = "(INSERT|insert)\s((INTO|into)\s)?[A-Za-z_\d]+\s\(([\"A-Za-z\d],?\s?)+\)"
+    if re.match(pattern, command):
+        command_name = command.split()[0]
+        insert_data = find_between(command, "(", ")").split(", ")
+        for data in range(len(insert_data)):
+            insert_data[data] = find_between(insert_data[data], "\"", "\"")
+        print("ok")
+        return command_name, insert_data
+    else:
+        print("wrong input")
+
+name, data = read_insert("INSERT cats (\"2\", \"Pushok\", \"Fish\")")
+
+print(name, data)
+
+
