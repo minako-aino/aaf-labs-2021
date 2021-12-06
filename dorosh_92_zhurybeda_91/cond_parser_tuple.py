1aa6fb6e7b7ebe5b151f8bd88fa39d2e4d55d45e
Avard3 <iurriy.zhur@gmail.com>
2021-12-06 15:37:50 +0200

    add preorder travers for cond tree

--- dorosh_92_zhurybeda_91/cond_parser_tuple.py
+++ dorosh_92_zhurybeda_91/cond_parser_tuple.py
@@ -5,19 +5,22 @@ class Node:
     def __init__(self, token_type, value=None):
         self.token_type = token_type
         self.value = value
-        self.children = []
+        self.right = None
+        self.left = None
 
 
 def match(tokens, token):
     if tokens[0][1] == token:
-        return tokens.pop(0)
+        return tokens.pop(0)[0]
     else:
         raise Exception('Invalid syntax on token {}'.format(tokens[0][1]))
 
 
 def parse_e3(tokens):
     if tokens[0][1] in ["T_STR", "T_VALUE"]:
-        return tokens.pop(0)
+        node = Node(tokens[0][1], value=tokens[0][0])
+        tokens.pop(0)[0]
+        return node
 
     match(tokens, "T_LPAR")
     expression = parse_e(tokens)
@@ -31,9 +34,9 @@ def parse_e2(tokens):
 
     while tokens[0][1] in ["T_OR", "T_AND"]:
         node = Node(tokens[0][1], value=tokens[0][0])
-        tokens.pop(0)
-        node.children.append(left_node)
-        node.children.append(parse_e3(tokens))
+        tokens.pop(0)[1]
+        node.left = left_node
+        node.right = parse_e3(tokens)
         left_node = node
 
     return left_node
@@ -50,9 +53,9 @@ def parse_e(tokens):
                            "T_LESS_EQ"
                            ]:
         node = Node(tokens[0][1], value=tokens[0][0])
-        tokens.pop(0)
-        node.children.append(left_node)
-        node.children.append(parse_e2(tokens))
+        tokens.pop(0)[0]
+        node.left = left_node
+        node.right = parse_e2(tokens)
         left_node = node
 
     return left_node
@@ -80,12 +83,24 @@ def print_ast(ast):
         _print_ast(ast.value)
 
 
+def printPreorder(root):
+    if root:
+        # First print the data of node
+        print(root.value),
+
+        # Then recur on left child
+        printPreorder(root.left)
+
+        # Finally recur on right child
+        printPreorder(root.right)
 # com = imp_lex('((name <= "C") OR (name >= "X"))')
 # ast = parse(com)
-# # print(type(ast) == Node)
-# # print_ast(ast)
-# # ast = parse('(name <= "Murzik") Or (name = "Pushok")')
-# print(ast.value)
+# print(ast)
+
+# com = imp_lex('(name <= "Murzik") OR (name = "Pushok") AND (dog = "Shiba")')
+# ast = parse(com)
+# print(ast)
+# printPreorder(ast)
 # print(ast.children[0].value)
 # print(ast.children[0].children[0])
 # print(ast.children[0].children[1])
